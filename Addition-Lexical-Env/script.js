2a211c61ed19c0ba8dbf62b77edb04c7fe0e7e05
DFBlok <s219392862@mandela.ac.za>
2024-03-14 14:26:05 +0200

    exercise 3 counter

--- Addition-Lexical-Env/script.js
+++ Addition-Lexical-Env/script.js
@@ -1,12 +1,12 @@
 //variable_shadowing--------start
-/* let globalVar = "I am global"
+let globalVar = "I am global"
 
 function shadowingExample(){
     let localVar = "I am local";
     console.log(globalVar);
     console.log(localVar)
 }
-shadowingExample(); */
+shadowingExample();
 //Chain scope exercise
 function outerFunction(){
     let outerVariable = 'Outer variable';
@@ -26,4 +26,31 @@ function outerFunction(){
     middleFunction();
 }
 outerFunction();
+//exercise
+function createCount(){
+    let count =0;
+    function increment(){
+        count++;
+        console.log(count);
+    }
+
+    function decrease(){
+        count--;
+        console.log(count);
+    }
+
+    return{
+        increment: increment,
+        decrease: decrease
+    } ;
+}
+
+let counter1 = createCount();
+let counter2 = createCount();
+counter1.increment();
+counter1.increment();
+counter2.decrease();
+counter1.increment();
+counter1.decrease();
+
 
